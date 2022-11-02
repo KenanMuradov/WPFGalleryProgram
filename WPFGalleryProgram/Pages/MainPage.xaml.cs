27e9a9161303fa4d187140ded12846e5fd490b50
KenanMuradov <maljerov.1999@gmail.com>
2022-11-03 00:09:26 +0300

    Semi Final

--- WPFGalleryProgram/Pages/MainPage.xaml.cs
+++ WPFGalleryProgram/Pages/MainPage.xaml.cs
@@ -1,17 +1,15 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFGalleryProgram.Pages
 {
@@ -25,6 +23,7 @@ namespace WPFGalleryProgram.Pages
 
             ImageSources = new();
         }
+
         private void lbx_DragOver(object sender, DragEventArgs e)
         {
             bool dropEnabled = true;
@@ -33,9 +32,9 @@ namespace WPFGalleryProgram.Pages
                 string[] filenames =
                                  e.Data.GetData(DataFormats.FileDrop, true) as string[];
 
-                foreach (string filename in filenames)
+                foreach (string fileName in filenames)
                 {
-                    if (System.IO.Path.GetExtension(filename).ToUpperInvariant() != ".JPG" && System.IO.Path.GetExtension(filename).ToUpperInvariant() != ".JPEG" && System.IO.Path.GetExtension(filename).ToUpperInvariant() != ".PNG")
+                    if (System.IO.Path.GetExtension(fileName).ToUpperInvariant() != ".JPG" && System.IO.Path.GetExtension(fileName).ToUpperInvariant() != ".JPEG" && System.IO.Path.GetExtension(fileName).ToUpperInvariant() != ".PNG")
                     {
                         dropEnabled = false;
                         break;
@@ -84,9 +83,72 @@ namespace WPFGalleryProgram.Pages
                 PhotoPage photoPage = new(image.Source, ImageSources);
 
                 NavigationService.Navigate(photoPage);
+            }
+        }
 
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "Images|*.png;*.jpg;*.jpeg"
+            };
+
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var image = new Image()
+                {
+                    Source = new BitmapImage(new Uri(openFileDialog.FileName)),
+                    Width = 100,
+                    Height = 100,
+                    MinHeight = 70,
+                    MinWidth = 70,
+                    Stretch = Stretch.Uniform
+                };
+
+                lbx.Items.Add(image);
+
+                ImageSources.Add(image.Source);
             }
 
         }
+
+        private void OpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog dialog = new();
+
+
+            if (dialog.ShowDialog() == true)
+            {
+                var directoryInfo = new DirectoryInfo(dialog.SelectedPath);
+
+                foreach (var file in directoryInfo.GetFiles())
+                {
+                    string fileName = file.Name;
+
+                    if (Path.GetExtension(fileName).ToUpperInvariant() == ".JPG" || Path.GetExtension(fileName).ToUpperInvariant() == ".JPEG" || Path.GetExtension(fileName).ToUpperInvariant() == ".PNG")
+                    {
+
+                        var image = new Image()
+                        {
+                            Source = new BitmapImage(new Uri(file.FullName)),
+                            Width = 100,
+                            Height = 100,
+                            MinHeight = 70,
+                            MinWidth = 70,
+                            Stretch = Stretch.Uniform
+                        };
+
+                        lbx.Items.Add(image);
+
+                        ImageSources.Add(image.Source);
+
+                    }
+                }
+            }
+
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e) => MessageBox.Show("Will Be Updated Soon");
     }
 }
--- WPFGalleryProgram/Pages/PhotoPage.xaml.cs
+++ WPFGalleryProgram/Pages/PhotoPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,20 +15,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WPFGalleryProgram.Pages;
 
 public partial class PhotoPage : Page, INotifyPropertyChanged
 {
 
+    bool isAutoPlayActice = false;
+
     private ImageSource _currentImageSource;
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
-    protected void OnPropertyChanged([CallerMemberName] string? name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
+    protected void OnPropertyChanged([CallerMemberName] string? name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     public ImageSource CurrentImageSource
     {
         get { return _currentImageSource; }
@@ -48,6 +49,22 @@ public partial class PhotoPage : Page, INotifyPropertyChanged
         DataContext = this;
         _currentImageSource = currentImageSource;
         ImageSources = imageSources;
+    }
+
+
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        dispatcherTimer.Tick += DispatcherTimer_Tick; ;
+        dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+        dispatcherTimer.Start();
+
+    }
+
+    private void DispatcherTimer_Tick(object? sender, EventArgs e)
+    {
+        if (isAutoPlayActice)
+            BtnForward_Click(sender, new RoutedEventArgs());
     }
 
     private void BtnForward_Click(object sender, RoutedEventArgs e)
@@ -78,4 +95,15 @@ public partial class PhotoPage : Page, INotifyPropertyChanged
 
     private void BtnBack_Click(object sender, RoutedEventArgs e) => NavigationService.GoBack();
 
+    private void BtnAutoPlay_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button)
+        {
+            isAutoPlayActice = button.Name switch
+            {
+                "BtnPlay" => true,
+                _ => false
+            };
+        }
+    }
 }
