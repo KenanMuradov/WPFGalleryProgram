46049c46ae5e17fd4392f3d73b79a11d74996175
KenanMuradov <maljerov.1999@gmail.com>
2022-11-02 21:59:19 +0300

    Beta 1

---
+++ WPFGalleryProgram/Pages/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFGalleryProgram.Pages
+{
+    public partial class MainPage : Page
+    {
+        public List<ImageSource> ImageSources { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ImageSources = new();
+        }
+        private void lbx_DragOver(object sender, DragEventArgs e)
+        {
+            bool dropEnabled = true;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filenames =
+                                 e.Data.GetData(DataFormats.FileDrop, true) as string[];
+
+                foreach (string filename in filenames)
+                {
+                    if (System.IO.Path.GetExtension(filename).ToUpperInvariant() != ".JPG" && System.IO.Path.GetExtension(filename).ToUpperInvariant() != ".JPEG" && System.IO.Path.GetExtension(filename).ToUpperInvariant() != ".PNG")
+                    {
+                        dropEnabled = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                dropEnabled = false;
+            }
+
+            if (!dropEnabled)
+            {
+                e.Effects = DragDropEffects.None;
+                e.Handled = true;
+            }
+        }
+
+
+        private void lbx_Drop(object sender, DragEventArgs e)
+        {
+            foreach (var filename in e.Data.GetData(DataFormats.FileDrop) as string[])
+            {
+                var image = new Image()
+                {
+                    Source = new BitmapImage(new Uri(filename)),
+                    Width = 100,
+                    Height = 100,
+                    MinHeight = 70,
+                    MinWidth = 70,
+                    Stretch = Stretch.Uniform
+                };
+
+                lbx.Items.Add(image);
+
+                ImageSources.Add(image.Source);
+
+            }
+
+        }
+
+        private void lbx_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (lbx.SelectedItem is Image image)
+            {
+                PhotoPage photoPage = new(image.Source, ImageSources);
+
+                NavigationService.Navigate(photoPage);
+
+            }
+
+        }
+    }
+}
---
+++ WPFGalleryProgram/Pages/PhotoPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFGalleryProgram.Pages;
+
+public partial class PhotoPage : Page,INotifyPropertyChanged
+{
+
+    private ImageSource _currentImageSource;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+    public ImageSource CurrentImageSource
+    {
+        get { return _currentImageSource; }
+        set 
+        { 
+            _currentImageSource = value;
+            OnPropertyChanged();
+        }
+    }
+
+
+    public List<ImageSource> ImageSources { get; set; }
+
+
+    public PhotoPage(ImageSource currentImageSource, List<ImageSource> imageSources)
+    {
+        InitializeComponent();
+        DataContext = this;
+        _currentImageSource = currentImageSource;
+        ImageSources = imageSources;
+    }
+
+    private void BtnForward_Click(object sender, RoutedEventArgs e)
+    {
+        var index = ImageSources.IndexOf(CurrentImageSource);
+
+        if (index + 1 < ImageSources.Count)
+        {
+            CurrentImageSource = ImageSources[index + 1];
+            return;
+        }
+
+        CurrentImageSource = ImageSources[0];
+    }
+
+    private void BtnBackward_Click(object sender, RoutedEventArgs e)
+    {
+        var index = ImageSources.IndexOf(CurrentImageSource);
+
+        if (index -1 >= 0)
+        {
+            CurrentImageSource = ImageSources[index - 1];
+            return;
+        }
+
+        CurrentImageSource = ImageSources[ImageSources.Count-1];
+    }
+}
